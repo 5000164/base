cfb4e9d29d0c07638dbe3eccc6fd4bc6d4b25fb1
Hiroshi Sugawara <5000164@users.noreply.github.com>
2020-04-18 18:03:42 +0200

    refactor: unifying into arrow functions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,16 @@ import React from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { PlanList } from "./components/plan-list";
 
-function App() {
-  return (
-    <HelmetProvider>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>base</title>
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content="script-src 'self' 'unsafe-inline';"
-        />
-      </Helmet>
-      <PlanList />
-    </HelmetProvider>
-  );
-}
-
-export default App;
+export const App = () => (
+  <HelmetProvider>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>base</title>
+      <meta
+        httpEquiv="Content-Security-Policy"
+        content="script-src 'self' 'unsafe-inline';"
+      />
+    </Helmet>
+    <PlanList />
+  </HelmetProvider>
+);
