076b6ebb193803d3aefdb7bd21e2b3369f0adbe9
Hiroshi Sugawara <5000164@users.noreply.github.com>
2021-01-17 09:02:58 +0900

    refactor: Remove down migrations because it will not be used

--- server/src/migration.ts
+++ server/src/migration.ts
@@ -19,11 +19,6 @@ export const migrate = async ({ dbPath }: { dbPath: string }) => {
               );
           `);
         },
-        async down() {
-          sequelize.query(`
-              DROP TABLE tasks;
-          `);
-        },
       },
       {
         name: "01-add-status-column-to-tasks-table",
@@ -43,27 +38,6 @@ export const migrate = async ({ dbPath }: { dbPath: string }) => {
           sequelize.query(`
               INSERT INTO tasks (id, name, status)
               SELECT id, name, 0 AS status
-              FROM tasksTemp;
-          `);
-          sequelize.query(`
-              DROP TABLE tasksTemp;
-          `);
-        },
-        async down() {
-          sequelize.query(`
-              ALTER TABLE tasks
-                  RENAME TO tasksTemp;
-          `);
-          sequelize.query(`
-              CREATE TABLE tasks
-              (
-                  id   INTEGER PRIMARY KEY AUTOINCREMENT,
-                  name TEXT NOT NULL
-              );
-          `);
-          sequelize.query(`
-              INSERT INTO tasks (id, name)
-              SELECT id, name
               FROM tasksTemp;
           `);
           sequelize.query(`
@@ -96,28 +70,6 @@ export const migrate = async ({ dbPath }: { dbPath: string }) => {
               DROP TABLE tasksTemp;
           `);
         },
-        async down() {
-          sequelize.query(`
-              ALTER TABLE tasks
-                  RENAME TO tasksTemp;
-          `);
-          sequelize.query(`
-              CREATE TABLE tasks
-              (
-                  id     INTEGER PRIMARY KEY AUTOINCREMENT,
-                  name   TEXT    NOT NULL,
-                  status INTEGER NOT NULL
-              );
-          `);
-          sequelize.query(`
-              INSERT INTO tasks (id, name, status)
-              SELECT id, name, status
-              FROM tasksTemp;
-          `);
-          sequelize.query(`
-              DROP TABLE tasksTemp;
-          `);
-        },
       },
       {
         name: "03-add-actual-column-to-tasks-table",
@@ -139,29 +91,6 @@ export const migrate = async ({ dbPath }: { dbPath: string }) => {
           sequelize.query(`
               INSERT INTO tasks (id, name, status, estimate, actual)
               SELECT id, name, status, estimate, NULL as actual
-              FROM tasksTemp;
-          `);
-          sequelize.query(`
-              DROP TABLE tasksTemp;
-          `);
-        },
-        async down() {
-          sequelize.query(`
-              ALTER TABLE tasks
-                  RENAME TO tasksTemp;
-          `);
-          sequelize.query(`
-              CREATE TABLE tasks
-              (
-                  id       INTEGER PRIMARY KEY AUTOINCREMENT,
-                  name     TEXT    NOT NULL,
-                  status   INTEGER NOT NULL,
-                  estimate INTEGER
-              );
-          `);
-          sequelize.query(`
-              INSERT INTO tasks (id, name, status, estimate)
-              SELECT id, name, status, estimate
               FROM tasksTemp;
           `);
           sequelize.query(`
@@ -196,30 +125,6 @@ export const migrate = async ({ dbPath }: { dbPath: string }) => {
               DROP TABLE tasksTemp;
           `);
         },
-        async down() {
-          sequelize.query(`
-              ALTER TABLE tasks
-                  RENAME TO tasksTemp;
-          `);
-          sequelize.query(`
-              CREATE TABLE tasks
-              (
-                  id       INTEGER PRIMARY KEY AUTOINCREMENT,
-                  name     TEXT    NOT NULL,
-                  status   INTEGER NOT NULL,
-                  estimate INTEGER,
-                  actual   INTEGER
-              );
-          `);
-          sequelize.query(`
-              INSERT INTO tasks (id, name, status, estimate, actual)
-              SELECT id, name, status, estimate, actual
-              FROM tasksTemp;
-          `);
-          sequelize.query(`
-              DROP TABLE tasksTemp;
-          `);
-        },
       },
       {
         name: "05-create-templates-table-and-template-tasks-table",
@@ -242,14 +147,6 @@ export const migrate = async ({ dbPath }: { dbPath: string }) => {
                   estimate   INTEGER,
                   FOREIGN KEY (templateId) REFERENCES templates (id)
               );
-          `);
-        },
-        async down() {
-          sequelize.query(`
-              DROP TABLE template_tasks;
-          `);
-          sequelize.query(`
-              DROP TABLE templates;
           `);
         },
       },
