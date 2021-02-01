5de613961657c2ab5a0d4d08cb42d14afbc27639
Hiroshi Sugawara <5000164@users.noreply.github.com>
2021-02-01 19:33:24 +0900

    chore: Make code simple

--- client/src/components/atoms/AnchorLink.tsx
+++ client/src/components/atoms/AnchorLink.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import { NavLink, NavLinkProps } from "react-router-dom";
 import { Anchor, AnchorProps } from "grommet/components/Anchor";
 
-export const AnchorLink: React.FC<AnchorLinkProps> = (props) => {
-  return (
-    <Anchor
-      as={({ colorProp, hasIcon, hasLabel, focus, ...p }) => <NavLink {...p} />}
-      {...props}
-    />
-  );
-};
-
 export type AnchorLinkProps = NavLinkProps &
   AnchorProps &
   Omit<JSX.IntrinsicElements["a"], "color">;
+
+export const AnchorLink: React.FC<AnchorLinkProps> = (props) => (
+  <Anchor
+    as={({ colorProp, hasIcon, hasLabel, focus, ...p }) => <NavLink {...p} />}
+    {...props}
+  />
+);
