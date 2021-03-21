dc0dd794ef33839a8b25548e51494078a67ea1b5
Hiroshi Sugawara <5000164@users.noreply.github.com>
2021-03-21 10:04:15 +0900

    feat: Improve the look of the task track page

--- client/src/components/molecules/TaskTrackListItem.tsx
+++ client/src/components/molecules/TaskTrackListItem.tsx
@@ -19,28 +19,34 @@ export const TaskTrackListItem = ({
   setStartAt: (startAt: string) => void;
   setStopAt: (stopAt: string) => void;
 }) => {
-  const { client } = React.useContext(AppContext);
+  const { client, date } = React.useContext(AppContext);
   const { reload } = React.useContext(TaskTracksPageContext);
 
   return (
     <StyledTaskTrackListItem>
       <Text size="small">{taskTrack.task.name}</Text>
-      {taskTrack.stop_at && (
+      {taskTrack.stop_at ? (
         <ElapsedTime seconds={taskTrack.stop_at - taskTrack.start_at} />
+      ) : (
+        <StyledWorking>Working</StyledWorking>
       )}
-      <TextInput
-        type="datetime-local"
-        value={format(taskTrack.start_at)}
-        onChange={(e) => setStartAt(e.target.value)}
+      <StyledTextInput
+        type="time"
+        step="1"
+        value={formatToTime(taskTrack.start_at)}
+        onChange={(e) => setStartAt(formatToDatetime(date, e.target.value))}
         onBlur={() => updateTaskTrack(client, taskTrack).then()}
       />
-      {taskTrack.stop_at && (
-        <TextInput
-          type="datetime-local"
-          value={format(taskTrack.stop_at)}
-          onChange={(e) => setStopAt(e.target.value)}
+      {taskTrack.stop_at ? (
+        <StyledTextInput
+          type="time"
+          step="1"
+          value={formatToTime(taskTrack.stop_at)}
+          onChange={(e) => setStopAt(formatToDatetime(date, e.target.value))}
           onBlur={() => updateTaskTrack(client, taskTrack).then()}
         />
+      ) : (
+        <div />
       )}
       <Button
         label="Delete"
@@ -56,31 +62,51 @@ export const TaskTrackListItem = ({
   );
 };
 
-const format = (time?: number) => {
-  if (time) {
-    const date = new Date(time * 1000);
-    return [
-      date.getFullYear().toString().padStart(4, "0"),
-      "-",
-      (date.getMonth() + 1).toString().padStart(2, "0"),
-      "-",
-      date.getDate().toString().padStart(2, "0"),
-      "T",
-      date.getHours().toString().padStart(2, "0"),
-      ":",
-      date.getMinutes().toString().padStart(2, "0"),
-      ":",
-      date.getSeconds().toString().padStart(2, "0"),
-    ].join("");
-  } else {
-    return "";
-  }
+const formatToTime = (time: number) => {
+  const date = new Date(time * 1000);
+  return [
+    date.getHours().toString().padStart(2, "0"),
+    ":",
+    date.getMinutes().toString().padStart(2, "0"),
+    ":",
+    date.getSeconds().toString().padStart(2, "0"),
+  ].join("");
+};
+
+const formatToDatetime = (dateString: string, timeString: string) => {
+  const times = timeString.split(":").map((t) => parseInt(t, 10));
+  const date = new Date(
+    new Date(Date.parse(dateString)).setHours(times[0], times[1], times[2], 0)
+  );
+  return [
+    date.getFullYear().toString().padStart(4, "0"),
+    "-",
+    (date.getMonth() + 1).toString().padStart(2, "0"),
+    "-",
+    date.getDate().toString().padStart(2, "0"),
+    "T",
+    date.getHours().toString().padStart(2, "0"),
+    ":",
+    date.getMinutes().toString().padStart(2, "0"),
+    ":",
+    date.getSeconds().toString().padStart(2, "0"),
+  ].join("");
 };
 
 const StyledTaskTrackListItem = styled.li`
   display: grid;
   align-items: center;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: 1fr 80px repeat(2, 120px) 80px;
   grid-gap: 5px;
   margin: 5px 0;
+`;
+
+const StyledWorking = styled.div`
+  text-align: right;
+`;
+
+const StyledTextInput = styled(TextInput)`
+  &::-webkit-calendar-picker-indicator {
+    filter: invert(1);
+  }
 `;
